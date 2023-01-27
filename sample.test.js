2e9ecb81d8999d52350816e8448aab3e153a0c95
CarlosRuiz <carua97@gmail.com>
2023-01-27 19:18:19 +0100

    Test try sonar

--- sample.test.js
+++ sample.test.js
@@ -3,8 +3,16 @@ import { exampleFunc } from './sample';
 describe('Given funct example', () => {
   describe('When the function is X', () => {
     test('Then it gives the same value', () => {
-      const r = exampleFunc(2);
-      expect(r).toEqual(2);
+      const r = exampleFunc(3);
+      expect(r).toEqual(3);
+    });
+    test('Then it gives the same value', () => {
+      const r = exampleFunc(5);
+      expect(r).toBe(5);
+    });
+    test('Then it gives the same value', () => {
+      const r = exampleFunc('x');
+      expect(r).toEqual('x');
     });
   });
 });
